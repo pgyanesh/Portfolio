c18a7662d4f4a55336ee5daa430989a544545e0e
Gyanesh kumar padhiary <gyaneshp.dev@gmail.com>
2023-10-27 23:28:49 +0530

    Making changes to the skills section

--- src/Pages/ThirdPage/ThirdPage.jsx
+++ src/Pages/ThirdPage/ThirdPage.jsx
@@ -16,16 +16,16 @@ const ThirdPage = () => {
             <h1>Skills</h1>
         </div>
         <div className="thirdPage-skills">
-          <ul>
-            <li className='skills-li'><FaHtml5/></li>
-            <li className='skills-li'><FaCss3Alt/></li>
-            <li className='skills-li'><FaJsSquare/></li>
-            <li className='skills-li'><FaReact/></li>
-            <li className='skills-li'><FaNode/></li>
-            <li className='skills-li'><SiMongodb/></li>
-            <li className='skills-li'><SiMysql/></li>
-            <li className='skills-li'><SiRedux/></li>
-          </ul>
+         
+            <div className="skills"><FaHtml5 className='icons'/><span className='skill-span'>Html</span></div>
+            <div className="skills"><FaCss3Alt className='icons'/><span className='skill-span'>Css</span></div>
+            <div className="skills"><FaJsSquare className='icons'/><span className='skill-span'>Javascript</span></div>
+            <div className="skills"><FaReact className='icons'/><span className='skill-span'>React</span></div>
+            <div className="skills"><FaNode className='icons'/><span className='skill-span'>Node Js</span></div>
+          <div className="skills"><SiMongodb className='icons'/><span className='skill-span'>Mongodb</span></div>
+           <div className="skills"><SiMysql className='icons'/><span className='skill-span'>MySql</span></div>
+            <div className="skills"><SiRedux className='icons'/><span className='skill-span'>Redux</span></div>
+          
         </div>
     </div>
     </>
